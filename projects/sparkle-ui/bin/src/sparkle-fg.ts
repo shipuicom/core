c639dcef1550b70c8e02feaa0030459ff0fee2c4
sp90 <dev@nomis.dk>
2024-07-03 10:17:34 +0200

    Add file watcher for the icon library

--- projects/sparkle-ui/bin/src/sparkle-fg.ts
+++ projects/sparkle-ui/bin/src/sparkle-fg.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env bun
 
 import { Glob } from 'bun';
+import { watch } from 'fs';
 import { resolve } from 'path';
 import subsetFont from 'subset-font';
 import { mapUnicodesToGlyphs } from './prep-font';
@@ -9,14 +10,14 @@ type SupportedFontTypes = 'woff' | 'woff2' | 'ttf';
 export type InputArguments = {
   src: string;
   out: string;
-  rootPath?: string;
+  rootPath: string;
+  watch: boolean;
+  watchLib: boolean;
+  verbose: boolean;
 };
 
-export const main = async (values: InputArguments) => {
+const run = async (PROJECT_SRC: string, LIB_SRC: string, PROJECT_PUBLIC: string, values: InputArguments) => {
   const startTime = performance.now();
-  const LIB_SRC = resolve(import.meta.dir, '../../lib');
-  const PROJECT_SRC = values.src;
-  const PROJECT_PUBLIC = values.out;
   const TARGET_FONT_TYPE: SupportedFontTypes = 'woff2' as SupportedFontTypes;
 
   function formatFileSize(bytes: number, dm = 2) {
@@ -159,6 +160,8 @@ const iconsSnippetContent = `
 
   const compressedFont = Bun.gzipSync(subsetBuffer);
   const compressedCss = Bun.gzipSync(cssFileContent);
+
+  if (values.verbose) {
   console.log('Generated font file size: ', formatFileSize(fontWrites));
   console.log('Generated css file size: ', formatFileSize(cssWrites));
   console.log('Generated total file size: ', formatFileSize(fontWrites + cssWrites));
@@ -167,5 +170,37 @@ const iconsSnippetContent = `
   console.log('Generated total compressed file size: ', formatFileSize(compressedFont.length + compressedCss.length));
   console.log('Generated types file size: ', formatFileSize(iconsTsWrites));
   console.log('Time taken: ', runtime.toFixed(2) + 'ms');
-  console.log(' ');
+} else {
+  console.log(`Generated/Compressed size: ${formatFileSize(fontWrites + cssWrites)}/${formatFileSize(compressedFont.length + compressedCss.length)}`);
+  console.log('Time taken: ', runtime.toFixed(2) + 'ms');
+}
+console.log(' ');
+}
+
+export const main = async (values: InputArguments) => {
+  const LIB_SRC = resolve(import.meta.dir, '../../lib');
+  const PROJECT_SRC = values.src;
+  const PROJECT_PUBLIC = values.out;
+
+  if (values.watch) {
+    watch(
+      PROJECT_SRC,
+      { recursive: true },
+      () => {
+        run(PROJECT_SRC, LIB_SRC, PROJECT_PUBLIC, values);
+      },
+    );
+
+    if (values.watchLib) {
+      watch(
+        LIB_SRC,
+        { recursive: true },
+        () => {
+          run(PROJECT_SRC, LIB_SRC, PROJECT_PUBLIC, values);
+        },
+      );
+    }
+  } else {
+    run(PROJECT_SRC, LIB_SRC, PROJECT_PUBLIC, values);
+  }
 };
