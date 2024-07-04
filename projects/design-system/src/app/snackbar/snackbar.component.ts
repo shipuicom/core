3e2b832982306d4d52179ca89a7fe53dca17eaf3
sp90 <dev@nomis.dk>
2024-07-04 10:35:21 +0200

    Patch release

--- projects/design-system/src/app/snackbar/snackbar.component.ts
+++ projects/design-system/src/app/snackbar/snackbar.component.ts
@@ -3,35 +3,35 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { SparkleAlertModule, SparkleAlertService } from '@sparkle-ui/core';
+import { SparkleAlertComponent, SparkleAlertContainerComponent, SparkleAlertService } from '../../../../sparkle-ui/src/public-api';
 
 @Component({
   selector: 'app-snackbar',
   standalone: true,
-  imports: [MatSnackBarModule, MatIconModule, MatButtonModule, MatDividerModule, SparkleAlertModule],
+  imports: [MatSnackBarModule, MatIconModule, MatButtonModule, MatDividerModule, SparkleAlertComponent, SparkleAlertContainerComponent],
   templateUrl: './snackbar.component.html',
   styleUrl: './snackbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class SnackbarComponent {
-  #sparkleAlertService = inject(SparkleAlertService);
+  sparkleAlertService = inject(SparkleAlertService);
 
   count = 0;
 
   ngOnInit() {
-    this.#sparkleAlertService.addAlert({
+    this.sparkleAlertService.addAlert({
       type: 'error',
       title: 'Error',
       content: 'This is an error',
     });
-    this.#sparkleAlertService.addAlert({
+    this.sparkleAlertService.addAlert({
       type: 'primary',
       title: 'Info',
       content: 'This is an info',
     });
 
     setTimeout(() => {
-      this.#sparkleAlertService.addAlert({
+      this.sparkleAlertService.addAlert({
         type: 'primary',
         title: 'Short info',
       });
@@ -41,7 +41,7 @@ export default class SnackbarComponent {
   addAlert() {
     this.count += 1;
 
-    this.#sparkleAlertService.addAlert({
+    this.sparkleAlertService.addAlert({
       type: 'question',
       title: 'Yet another question? - ' + this.count,
     });
