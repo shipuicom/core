310e9fe2c956f52f37452842ea50bb6c2baa4afe
sp90 <dev@nomis.dk>
2024-09-20 13:22:07 +0200

    Improve on the divider to enable label that inherits background color, font and color

--- projects/design-system/src/app/sparkle/spk-divider/spk-divider.component.ts
+++ projects/design-system/src/app/sparkle/spk-divider/spk-divider.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SparkleDividerComponent } from '@sparkle-ui/core';
 
 @Component({
   selector: 'app-spk-divider',
   standalone: true,
-  imports: [],
+  imports: [SparkleDividerComponent],
   templateUrl: './spk-divider.component.html',
   styleUrl: './spk-divider.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
