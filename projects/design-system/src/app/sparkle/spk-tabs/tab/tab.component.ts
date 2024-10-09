3d2107c1dc609f8969b6ac3bc53e214bceb9fe95
sp90 <dev@nomis.dk>
2024-10-09 15:55:22 +0200

    Add optional cdk overlay styles

--- projects/design-system/src/app/sparkle/spk-tabs/tab/tab.component.ts
+++ projects/design-system/src/app/sparkle/spk-tabs/tab/tab.component.ts
@@ -1,13 +1,31 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, TemplateRef, viewChild } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { SparkleButtonComponent } from '@sparkle-ui/core';
 
 @Component({
   selector: 'app-tab',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, SparkleButtonComponent],
   templateUrl: './tab.component.html',
   styleUrl: './tab.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class TabComponent {
+  #dialog = inject(MatDialog);
+
   id = input.required();
+
+  myDialog = viewChild.required<TemplateRef<any>>('myDialog');
+
+  openDialog() {
+    const dialogRef = this.#dialog.open(this.myDialog(), {
+      width: '500px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        console.log('result: ', result);
+      }
+    });
+  }
 }
