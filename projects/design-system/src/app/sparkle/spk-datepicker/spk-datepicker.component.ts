4692fc0776c3775a04dd4fb789a6fadf0e42144d
sp90 <dev@nomis.dk>
2024-10-17 10:55:57 +0200

    Support multiple styles and colors

--- projects/design-system/src/app/sparkle/spk-datepicker/spk-datepicker.component.ts
+++ projects/design-system/src/app/sparkle/spk-datepicker/spk-datepicker.component.ts
@@ -1,13 +1,13 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { SparkleDatepickerComponent } from '../../../../../sparkle-ui/src/public-api';
+import { SparkleButtonGroupComponent, SparkleDatepickerComponent } from '../../../../../sparkle-ui/src/public-api';
 
 const now = new Date();
 
 @Component({
   selector: 'app-spk-datepicker',
   standalone: true,
-  imports: [SparkleDatepickerComponent, FormsModule],
+  imports: [SparkleDatepickerComponent, SparkleButtonGroupComponent, FormsModule],
   templateUrl: './spk-datepicker.component.html',
   styleUrl: './spk-datepicker.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,6 +15,13 @@ const now = new Date();
 export default class SpkDatepickerComponent {
   someDate = signal(now);
   someOtherDate = signal(new Date('2023-01-01, 05:34:01'));
+
+  type = signal<'none' | 'raised' | 'outlined'>('none');
+  colors = signal<'none' | 'primary' | 'accent' | 'tertiary' | 'warn' | 'success'>('primary');
+
+  class = computed(() => {
+    return `${this.type()} ${this.colors()}`;
+  });
 
   updateTimeUTC(hh: number, mm: number, ss: number) {
     this.someOtherDate.set(
