3bf01ea289cda10096e2d9a42407ae74f9e06084
sp90 <dev@nomis.dk>
2025-06-24 11:00:18 +0200

    Add color schemes to steppers

--- projects/design-system/src/app/sparkle/spk-stepper/spk-stepper.component.ts
+++ projects/design-system/src/app/sparkle/spk-stepper/spk-stepper.component.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { SparkleRadioComponent, SparkleStepperComponent } from '../../../../../sparkle-ui/src/public-api';
+import {
+  SparkleButtonGroupComponent,
+  SparkleRadioComponent,
+  SparkleStepperComponent,
+} from '../../../../../sparkle-ui/src/public-api';
 
 @Component({
   selector: 'app-spk-stepper',
-  imports: [SparkleStepperComponent, SparkleRadioComponent, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [
+    SparkleStepperComponent,
+    SparkleRadioComponent,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    SparkleButtonGroupComponent,
+  ],
   templateUrl: './spk-stepper.component.html',
   styleUrl: './spk-stepper.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SpkStepperComponent {}
+export default class SpkStepperComponent {
+  colorClass = signal<'' | 'primary' | 'accent' | 'warn' | 'error' | 'success'>('primary');
+}
