a90af497c5f46ecf3f9048c8070ab8e5a0e4f7a9
sp90 <dev@nomis.dk>
2025-06-24 11:38:33 +0200

    Update docs for file upload

--- projects/design-system/src/app/sparkle/spk-file-upload/spk-file-upload.component.ts
+++ projects/design-system/src/app/sparkle/spk-file-upload/spk-file-upload.component.ts
@@ -1,20 +1,21 @@
-import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
-import { FormBuilder, FormsModule } from '@angular/forms';
-import { SparkleFileUploadComponent, SparkleIconComponent } from '../../../../../sparkle-ui/src/public-api';
-
-const fb = new FormBuilder();
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PreviewerComponent } from '../../previewer/previewer.component';
+import { PropertyViewerComponent } from '../../property-viewer/property-viewer.component';
+import { BaseFileUploadComponent } from './examples/base-file-upload/base-file-upload.component';
+import { FileUploadSandboxComponent } from './examples/file-upload-sandbox/file-upload-sandbox.component';
 
 @Component({
   selector: 'app-spk-file-upload',
-  imports: [FormsModule, SparkleFileUploadComponent, SparkleIconComponent],
+  imports: [
+    FormsModule,
+    PropertyViewerComponent,
+    PreviewerComponent,
+    FileUploadSandboxComponent,
+    BaseFileUploadComponent,
+  ],
   templateUrl: './spk-file-upload.component.html',
   styleUrl: './spk-file-upload.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SpkFileUploadComponent {
-  files = signal<File[]>([]);
-
-  fileEffect = effect(() => {
-    console.log('files: ', this.files());
-  });
-}
+export default class SpkFileUploadComponent {}
