70451d6c7b3c28a12323ae487ed2c32e37194a87
sp90 <dev@nomis.dk>
2025-06-26 14:23:25 +0200

    Add docs for sortables

--- projects/design-system/src/app/sparkle/spk-sortable/spk-sortable.component.ts
+++ projects/design-system/src/app/sparkle/spk-sortable/spk-sortable.component.ts
@@ -1,12 +1,7 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import {
-  AfterDropResponse,
-  moveIndex,
-  SparkleCheckboxComponent,
-  SparkleIconComponent,
-  SparkleListComponent,
-  SparkleSortableDirective,
-} from '../../../../../sparkle-ui/src/public-api';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PreviewerComponent } from '../../previewer/previewer.component';
+import { PropertyViewerComponent } from '../../property-viewer/property-viewer.component';
+import { BaseSortableComponent } from './examples/base-sortable/base-sortable.component';
 
 const CONTENT_EXAMPLE = [
   {
@@ -44,75 +39,11 @@ const CONTENT_EXAMPLE = [
   },
 ];
 
-const TODOS = [
-  {
-    title: 'Simple sorting of list',
-    done: true,
-  },
-  {
-    title: 'Sorting animation',
-    done: true,
-  },
-  {
-    title: 'Support sortable handle',
-    done: true,
-  },
-  {
-    title: 'Support gap in sorting list ',
-    done: true,
-  },
-  {
-    title: 'Support placeholder',
-    done: true,
-  },
-  {
-    title: 'Support animation only when dragging',
-    done: true,
-  },
-  {
-    title: 'Support multiple lists',
-    done: false,
-  },
-  {
-    title: 'Support draggable grids',
-    done: false,
-  },
-];
-
-type ItemType = (typeof CONTENT_EXAMPLE)[0];
-type Todo = (typeof TODOS)[0];
-
 @Component({
   selector: 'app-spk-sortable',
-  imports: [SparkleListComponent, SparkleCheckboxComponent, SparkleIconComponent, SparkleSortableDirective],
+  imports: [BaseSortableComponent, PropertyViewerComponent, PreviewerComponent],
   templateUrl: './spk-sortable.component.html',
   styleUrl: './spk-sortable.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SpkSortableComponent {
-  items = signal(CONTENT_EXAMPLE);
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.items.update((items) => items.concat([CONTENT_EXAMPLE[0]]));
-    }, 800);
-  }
-
-  todos = signal(TODOS);
-
-  toggleTodo(index: number) {
-    this.todos.update((todos) => {
-      todos[index].done = !todos[index].done;
-
-      return todos;
-    });
-  }
-
-  reorderTodo(event: AfterDropResponse) {
-    this.todos.update((arr) => moveIndex<Todo>(arr, event));
-  }
-
-  reorder(event: AfterDropResponse) {
-    this.items.update((arr) => moveIndex<ItemType>(arr, event));
-  }
-}
+export default class SpkSortableComponent {}
