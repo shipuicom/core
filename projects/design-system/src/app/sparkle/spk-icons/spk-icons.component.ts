190a73ccc61ffc851f32e882558a94031877ea14
sp90 <dev@nomis.dk>
2025-07-01 12:37:32 +0200

    Improve icon docs

--- projects/design-system/src/app/sparkle/spk-icons/spk-icons.component.ts
+++ projects/design-system/src/app/sparkle/spk-icons/spk-icons.component.ts
@@ -1,12 +1,18 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { SparkleButtonComponent } from '../../../../../sparkle-ui/src/public-api';
+import { SparkleButtonComponent, SparkleIconComponent } from '../../../../../sparkle-ui/src/public-api';
 import { PreviewerComponent } from '../../previewer/previewer.component';
 import { PropertyViewerComponent } from '../../property-viewer/property-viewer.component';
 import { SandboxIconComponent } from './examples/sandbox-icon/sandbox-icon.component';
 
 @Component({
   selector: 'app-spk-icons',
-  imports: [PreviewerComponent, PropertyViewerComponent, SandboxIconComponent, SparkleButtonComponent],
+  imports: [
+    PreviewerComponent,
+    PropertyViewerComponent,
+    SandboxIconComponent,
+    SparkleButtonComponent,
+    SparkleIconComponent,
+  ],
   templateUrl: './spk-icons.component.html',
   styleUrl: './spk-icons.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
