ca92cd5f8e8792f9ac1acb53d6ad91095dd89e61
sp90 <dev@nomis.dk>
2025-08-18 10:47:57 +0200

    Improve docs for chips

--- projects/design-system/src/app/ship/chips/examples/chip-sandbox/chip-sandbox.component.ts
+++ projects/design-system/src/app/ship/chips/examples/chip-sandbox/chip-sandbox.component.ts
@@ -1,20 +1,56 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
-import { ShipButtonGroupComponent, ShipChipComponent, ShipIconComponent, ShipToggleComponent } from 'ship-ui';
+import {
+  ShipButtonGroupComponent,
+  ShipChipComponent,
+  ShipColorPickerComponent,
+  ShipIconComponent,
+  ShipToggleComponent,
+} from 'ship-ui';
 
 @Component({
   selector: 'app-chip-sandbox',
-  imports: [ShipIconComponent, ShipChipComponent, ShipButtonGroupComponent, ShipToggleComponent],
+  imports: [
+    ShipIconComponent,
+    ShipChipComponent,
+    ShipButtonGroupComponent,
+    ShipToggleComponent,
+    ShipColorPickerComponent,
+    JsonPipe,
+  ],
   templateUrl: './chip-sandbox.component.html',
   styleUrl: './chip-sandbox.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChipSandboxComponent {
   isSmall = signal<boolean>(false);
+  isSharp = signal<boolean>(false);
+  isDynamic = signal<boolean>(false);
   hasIcon = signal<boolean>(true);
   hasSuffixIcon = signal<boolean>(true);
   hasText = signal<boolean>(true);
 
   colorClass = signal<'' | 'primary' | 'accent' | 'warn' | 'error' | 'success'>('primary');
   variationClass = signal<'' | 'simple' | 'outlined' | 'flat' | 'raised'>('raised');
-  exampleClass = computed(() => this.colorClass() + ' ' + this.variationClass());
+  exampleClass = computed(() => {
+    if (this.isDynamic()) return '';
+
+    return this.colorClass() + ' ' + this.variationClass();
+  });
+
+  // Color picker
+  selectedColor = signal<[number, number, number]>([60, 131, 246]);
+  currentColor = signal<{ rgb: string; hex: string; hsl: string; hue: number; saturation: number } | null>(null);
+
+  // colorEffect = effect(() => {
+  //   if (this.isDynamic()) {
+  //     this.currentColor.set({
+  //       rgb: 'rgb(132,156,255)',
+  //       hex: '#849cff',
+  //       hsl: 'hsl(228, 100%, 76%)',
+  //       hue: 228,
+  //       saturation: 100,
+  //     });
+  //   }
+  // });
 }
