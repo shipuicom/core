fd66929f460627db1cfcd4a8b3d3382f1617383c
sp90 <dev@nomis.dk>
2025-08-29 14:09:33 +0200

    Bugfix button spacing with and without icons

--- projects/ship-ui/src/lib/ship-button/ship-button.component.ts
+++ projects/ship-ui/src/lib/ship-button/ship-button.component.ts
@@ -3,10 +3,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: '[shButton]',
   imports: [],
-  template: '<ng-content></ng-content>',
+  template: '<ng-content />',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'sh-sheet-h',
   },
 })
-export class ShipButtonComponent {}
+export class ShipButtonComponent {
+  // #selfRef = inject(ElementRef);
+  //   '[class.not-first-node-text]': 'notFirstNodeText()',
+  //   '[class.not-last-node-text]': 'notLastNodeText()',
+  // notFirstNodeText = signal<boolean>(false);
+  // notLastNodeText = signal<boolean>(false);
+  // ngOnInit() {
+  //   const childNodes = this.#selfRef.nativeElement.childNodes;
+  //   this.notFirstNodeText.set(childNodes[0].nodeType !== Node.TEXT_NODE);
+  //   this.notLastNodeText.set(childNodes[childNodes.length - 1].nodeType !== Node.TEXT_NODE);
+  // }
+}
